77d842e4133a35409fc1d81e06d2e069051808f2
Ajith Ganesh <vlavamithra@gmail.com>
2020-08-10 18:58:45 +0530

    new m2 method added

--- src/gitexample/Gitclass1.java
+++ src/gitexample/Gitclass1.java
@@ -8,12 +8,20 @@ public class Gitclass1 {
 		System.out.println("lava is in git");
 
 		m1();
+		m2();
+
 
 	}
 
 	public static void m1() {
 
 		System.out.println("in m1");
+	}
+
+	public static void m2(){
+
+		System.out.println("new m2 method");
+
 	}
 
 }
