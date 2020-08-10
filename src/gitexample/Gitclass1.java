dbd6a3a37d1e197ef06a99a53b35ce5e47dd483f
Ajith Ganesh <vlavamithra@gmail.com>
2020-08-10 11:15:40 +0530

    my first commit

---
+++ src/gitexample/Gitclass1.java
@@ -0,0 +1,19 @@
+package gitexample;
+
+public class Gitclass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("lava is in git");
+
+		m1();
+
+	}
+
+	public static void m1() {
+
+		System.out.println("in m1");
+	}
+
+}
---
+++ src/gitexample/Gitclass2.java
@@ -0,0 +1,22 @@
+package gitexample;
+
+public class Gitclass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Gitclass2 g1 =new Gitclass2();
+		
+		g1.mm();
+		
+	}
+
+	
+	public void mm() {
+		
+		
+		System.out.println("how is lava by using git tutorial");
+		
+	}
+}
